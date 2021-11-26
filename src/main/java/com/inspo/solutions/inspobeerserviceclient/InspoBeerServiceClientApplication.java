823d95a7df848572cf71412a6934ce9fe0b8c70f
Himadri Talukder <himadricuet@gmail.com>
2021-11-26 16:22:22 -0500

    Initial project structure created with rest template. closes #1 #2

---
+++ src/main/java/com/inspo/solutions/inspobeerserviceclient/InspoBeerServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.inspo.solutions.inspobeerserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspoBeerServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspoBeerServiceClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inspo/solutions/inspobeerserviceclient/InspoBeerServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspo.solutions.inspobeerserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspoBeerServiceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
